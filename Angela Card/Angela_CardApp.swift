4ac759745086e8ab4d764529c00abc5a49502010
Olivier Van hamme <nicefiction@gmail.com>
2021-04-01 19:45:29 +0200

    Initial Commit

---
+++ Angela Card/Angela_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Angela_CardApp.swift
+//  Angela Card
+//
+//  Created by Olivier Van hamme on 01/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct Angela_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Angela Card/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Angela Card
+//
+//  Created by Olivier Van hamme on 01/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
